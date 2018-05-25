474289d323f62caf6a7672e2b02a16f3cc5673bd
300756946 <rakter7@my.centennialcollege.ca>
2018-05-25 10:15:04 -0400

    Begin Craps example

---
+++ src/Week3/Ex4/Craps.java
@@ -0,0 +1,21 @@
+package Week3.Ex4;
+import java.security.SecureRandom;
+public class Craps {
+    //create the random number generator
+    private static final SecureRandom randomNumbers = new SecureRandom();
+    //create an enum for game states
+    private enum Status {
+      WON, LOST, CONTINUE;
+    };
+    //Static variables
+    private static final int SNAKE_EYES =2;
+    private static final int TREY =3;
+    private static final int SEVEN =7;
+    private static final int ELEVEN =11;
+    private static final int BOX_CARS =12;
+
+    public static void main(String[] args) {
+        //
+    }
+
+}
